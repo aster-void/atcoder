73ca88d42287cc9f2d3ebe1f7516966f17cb032e
aster <137767097+aster-void@users.noreply.github.com>
2024-09-07 22:49:55 +0900

    init flake+envrc

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        cargo-compete = import ./nix/cargo-compete.nix pkgs;
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            cargo-compete
+          ];
+
+          shellHook = ''
+            alias ccp="cargo compete"
+          '';
+        };
+      }
+    );
+}
