687256bfc226bf4b25c1383ab0fc7b7239352aeb
aster <137767097+aster-void@users.noreply.github.com>
2024-10-26 22:35:53 +0900

    complete abc377

--- flake.nix
+++ flake.nix
@@ -4,18 +4,20 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, fenix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         cargo-compete = import ./nix/cargo-compete.nix pkgs;
+        toolchain = import ./nix/fenix.nix { inherit system fenix; };
       in
       {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
-            cargo
+            toolchain
             cargo-compete
           ];
 
